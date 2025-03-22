1292ab9b0ea1dfefe95fc3ee59619a2667408a94
ssb-shree <bavachikarshree1666@gmail.com>
2025-03-22 19:03:29 +0530

    add routes to express app

--- src/app.js
+++ src/app.js
@@ -2,4 +2,10 @@ import express from "express";
 
 const app = express();
 
+// Import Routes Below
+import authRouter from "./routes/auth.routes.js";
+
+// Route Declaration Below
+app.use("/api/v1/auth", authRouter);
+
 export default app;
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/register", (req, res) => res.send("workssssssssss"));
+
+export default router;
