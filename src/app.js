537ea8096e8ed0e505b728fc41b05674beadf04f
ssb-shree <bavachikarshree1666@gmail.com>
2025-03-22 21:05:12 +0530

    minor changes to avoid problems added new global middlewares

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 import express from "express";
+import fileUpload from "express-fileupload";
+
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+// MiddleWares Below
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cookieParser());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/public/",
+  }),
+);
 
 // Import Routes Below
 import authRouter from "./routes/auth.routes.js";
 
 // Route Declaration Below
 app.use("/api/v1/auth", authRouter);
+
+app.get("/ping", async (req, res) => res.status(200).json({ message: "pong" }));
 
 export default app;
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 
 const router = Router();
 
-router.post("/register", (req, res) => res.send("workssssssssss"));
+import { registerUser } from "../controllers/auth.controller.js";
+
+router.post("/register", registerUser);
 
 export default router;
